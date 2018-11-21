cce248c6847517c301b92d9af76794221a666309
Grigoriy <wipmoon@gmail.com>
2018-11-21 11:35:52 +0700

    Сверстал главное меню и УТП с адаптивом

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
-$(function() {
+$(document).ready(function() {
 
+$('.humburger').on('click', function(e) {
+  e.preventDefault;
+  $(this).toggleClass('hum-active');
+  $('.menu').toggleClass('active');
+});
 
+});
 
-})
